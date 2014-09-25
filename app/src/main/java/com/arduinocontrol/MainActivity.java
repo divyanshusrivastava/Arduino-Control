2ae22da99201a893ce8264246d53432a2a293916
divyanshusrivastava <divyanshu.up32@gmail.com>
2014-09-25 12:26:11 +0530

    classes + layout added

--- app/src/main/java/com/arduinocontrol/MainActivity.java
+++ app/src/main/java/com/arduinocontrol/MainActivity.java
@@ -1,6 +1,7 @@
 package com.arduinocontrol;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,11 +9,13 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
 
     @Override
@@ -28,7 +31,7 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
             return true;
         }
         return super.onOptionsItemSelected(item);
